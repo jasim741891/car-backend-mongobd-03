481152963371906436ba6eaf1db4e7c9188f8319
jasim uddin <jasimgai74@gmail.com>
2021-10-30 08:43:43 +0600

    added port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const { MongoClient, Collection } = require('mongodb');
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 // medilWear
 app.use(cors());
 app.use(express.json())
